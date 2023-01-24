57251848b0aacf557d7bcb1256a2961483073b44
Brian Kukowski <briankukowski@gmail.com>
2023-01-24 10:52:40 -0500

    Added the jokes

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+jokes = [
+    "Joke 1:1",
+    "joke 2:2",
+    "joke 3:3",
+    "joke 4:4"
+
+]
+
 @app.get("/")
 def hello_world():
-    return "<p>Hello, World!<p>"
-
+    joke = random.choice(jokes)
+    return f"<p>{joke}<p>"
